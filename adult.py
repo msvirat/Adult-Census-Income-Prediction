ca18dc30b4ad2807ea5608e4a7a15db25514c5af
msvirat <msvirat@gmail.com>
2021-10-26 00:10:30 +0530

    Update adult.py
    Data preprocessing-Day 1

--- adult.py
+++ adult.py
@@ -5,42 +5,126 @@ Created on Sun Oct 24 08:57:16 2021
 @author: Naushina Farheen S
 """
 
-import pandas as pd # deals with data frame 
-import numpy as np  # deals with numerical values
+import pandas as pd# deals with data frame 
+import numpy as np# deals with numerical values
 import matplotlib.pyplot as plt # mostly used for visualization purposes 
-pip install dtale --user
+import seaborn as sns
 import dtale as dt
+import os
+'''
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+'''
+df = pd.read_csv("D:/Project/Adult-Census-Income-Prediction/adult.csv")# For Loading csv data.
 
-adult = pd.read_csv("C:/Users/Naushina Farheen S/Dropbox/My PC (DESKTOP-4RG72G3)/Desktop/NAUSHINA FOLDER/Practise Folder/adult.csv") # For Loading csv data.
 
-for col in adult.columns:
+
+for col in df.columns:
     print(col)
 
-adult = adult.rename({'education-num': 'educationnum', 'marital-status': 'maritalstatus', 'capital-gain': 'cg', 'capital-loss': 'cl', 'hours-per-week': 'hpw'}, axis=1)
+df = df.rename({'education-num': 'education_num', 'marital-status': 'marital_status', 'capital-gain': 'capital_gain', 'capital-loss': 'capital_loss', 'hours-per-week': 'hpw'}, axis=1)
 
-adult = pd.DataFrame(adult)
-adult.isnull().sum()
 
-adult.describe()
+df.replace(' ?', np.nan, inplace=True)
+df.isnull().mean()
+df.dropna(inplace=True)
 
-adult.var()
-adult.skew()
+
+for i in df.columns.values.tolist():
+    if (df[i].dtype == 'O'):
+        df[i] = df[i].str.strip()
+    else:
+        None
+
+
+for col in df.columns:
+    print(col)
+
+
+
+df['age'].describe()
+df['age'] = pd.cut(df['age'], np.array([15, 25, 45, 65, 100]), 4, labels=["Young", "Middle-age", "Senior", "Old"])
+
+
+
+df['workclass'].describe()
+df['workclass'].unique()
+df['workclass'] = df['workclass'].replace('State-gov', 'Government').replace('Local-gov', 'Government').replace('Federal-gov', 'Government')
+df['workclass'] = df['workclass'].replace('Self-emp-not-inc', 'Self').replace('Self-emp-inc', 'Self')
+
+
+
+df['fnlwgt'].describe()
+
+
+df['education'].describe()
+df['education'].unique()
+df['education'].value_counts()
+sns.histplot(df['education'])
+plt.plot(df['education'])
+df['education'] = df['education'].replace('Preschool', 'Below_College').replace('1st-4th', 'Below_College').replace('5th-6th', 'Below_College').replace('7th-8th', 'Below_College').replace('9th', 'Below_College').replace('10th', 'Below_College').replace('11th', 'Below_College').replace('12th', 'Below_College').replace('HS-grad', 'Below_College').replace('Assoc-acdm', 'Below_College').replace('Assoc-voc', 'Below_College')
+
+#Degree of college?
+
+#Preschool < 1st-4th < 5th-6th < 7th-8th < 9th < 10th < 11th < 12th < HS-grad < Prof-school < Assoc-acdm < Assoc-voc < Some-college < Bachelors < Masters < Doctorate.
+
+df['education_num'].describe()
+
+df['marital_status'].describe()
+df['marital_status'].unique()
+
+
+df['occupation'].describe()
+df['occupation'].unique()
+
+
+df['relationship'].describe()
+df['relationship'].unique()
+
+df['race'].describe()
+df['race'].unique()
+
+df['sex'].describe()
+df['sex'].unique()
+
+df['capital_gain'].describe()
+
+df['capital_loss'].describe()
+
+df['hpw'].describe()
+
+df['country'].describe()
+df['country'].unique()
+
+df['salary'].describe()
+
+
+
+
+
+df = pd.DataFrame(df)
+
+df.describe()
+
+df.var()
+df.skew()
 
 import dtale
-dtale.show(adult)
+dtale.show(df)
 d.open_browser()
 
 
 #### Dtale
-d=dt.show(adult)
+d=dt.show(df)
 d.open_browser()
 
-adult['occupation'].unique()
-adult['country'].unique()
+df['occupation'].unique()
+df['country'].unique()
 
-adult.mode()
+df.mode()
 
-plt.hist(adult["output variable"])  #right skewed =0.647
+plt.hist(df["output variable"])  #right skewed =0.647
 plt.hist(XYZ["input variable"])  #right skewed =0.858
 plt.boxplot(XYZ["output variable"],0,"rs",0) #Graphical representation for outliers present
 plt.boxplot(XYZ["input variable"],0,"rs",0) 
